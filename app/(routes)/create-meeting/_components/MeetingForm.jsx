9492de015d82d3165c8f5c30a36e5126e7d7e7da
Dhruv Ray <raydhruv25@gmail.com>
2025-02-21 10:27:34 -0600

    Changes made during 2/21/2025 class

--- app/(routes)/create-meeting/_components/MeetingForm.jsx
+++ app/(routes)/create-meeting/_components/MeetingForm.jsx
@@ -2,7 +2,7 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { ChevronLeft, Plus } from 'lucide-react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import {
     DropdownMenu,
@@ -17,9 +17,28 @@ import Image from 'next/image'
 import ThemeOptions from '@/app/_utils/ThemeOptions'
   
 
-function MeetingForm() {
-    const [location,setLocation]=useState(); 
+function MeetingForm({setFormValue}) {
     const [themeColor, setThemeColor]=useState();
+    const [eventName, setEventName]=useState();
+    const [duration, setDuration]=useState(30);
+    const [locationType, setLocationType]=useState();
+    const [locationUrl, setLocationUrl]=useState();
+
+
+
+
+    useEffect(()=>{
+        setFormValue({
+            eventName:eventName,
+            duration:duration, 
+            locationType:setLocationType,
+            locationUrl:locationUrl,
+            themeColor:themeColor
+
+        })
+
+    },[eventName, duration, setLocationType, locationUrl, themeColor])
+
 
   return (
     <div className='p-4'>
@@ -33,47 +52,49 @@ function MeetingForm() {
         </div>
         <div className='flex flex-col gap-3 my-4'>
             <h2 className='font-bold'>Event Name *</h2>
-            <Input placeholder='Name of your Meeting Event'/>
+            <Input placeholder='Name of your Meeting Event'
+            onChange={(event)=>setEventName(event.target.value)}
+            />
             <h2 className='font-bold'>Duration *</h2>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                    <Button className="max-w-40" variant="outline">30 mins</Button>
+                    <Button className="max-w-40" variant="outline">{duration}</Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem>15 Min</DropdownMenuItem>
-                    <DropdownMenuItem>30 Min</DropdownMenuItem>
-                    <DropdownMenuItem>45 Min</DropdownMenuItem>
-                    <DropdownMenuItem>60 Min</DropdownMenuItem>
+                    <DropdownMenuItem onClick={()=>setDuration(15)}>15 Min</DropdownMenuItem>
+                    <DropdownMenuItem onClick={()=>setDuration(30)}>30 Min</DropdownMenuItem>
+                    <DropdownMenuItem onClick={()=>setDuration(45)}>45 Min</DropdownMenuItem>
+                    <DropdownMenuItem onClick={()=>setDuration(60)}>60 Min</DropdownMenuItem>
                 </DropdownMenuContent>
                 </DropdownMenu>
 
             <h2 className='font-bold'>Location *</h2>
             <div className='grid grid-cols-4 gap-3'>
-                {LocationOption.map((option,index)=>(
+                {LocationOption.map((option, index)=>(
+
                     <div className={`border flex flex-col justify-center items-center p-3 rounded-lg
                     hover:bg-blue-100 hover:border-primary cursor-pointer
                     ${location==option.name&&'bg-blue-100 border-primary'}`}
-                    onClick={()=>setLocation(option.name)}>
+                    onClick={()=>setLocationType(option.name)}>
                         <Image src={option.icon} width={30} height={30} alt={option.name}/>
                         <h2>{option.name}</h2>
                     
                     </div>
 
-                )
-                
-                
-                )}
+                ))}
             </div>
-            {location&&<>
+            {locationType&&<>
             <h2 className='font-bold'>Add {location} URL</h2>
-            <Input placeholder='Add URL'/>
+            <Input placeholder='Add URL'
+            onChange={(event)=>setLocationUrl(event.target.value)}
+            />
             </>}
             <h2 className='font-bold'>Select Theme Color</h2>
             <div className='flex justify-evenly'>
                 {ThemeOptions.map((color,index)=>(
                     <div className={`h-7 w-7 rounded-full
-                    ${themeColor==color&&'border border-black'}`}
+                    ${themeColor==color&&'border-2 border-black'}`}
                     style={{backgroundColor:color}}
                     onClick={()=>setThemeColor(color)}
                     >
@@ -84,7 +105,9 @@ function MeetingForm() {
         
         </div>
 
-        <Button className="w-full mt-9">Create Meeting</Button>
+        <Button className="w-full mt-9"
+        disabled={(!eventName||!duration||!setLocationType||!locationUrl)}
+        >Create Meeting</Button>
   
     </div>
   )
--- app/(routes)/create-meeting/_components/PreviewMeeting.jsx
+++ app/(routes)/create-meeting/_components/PreviewMeeting.jsx
@@ -1,9 +1,32 @@
+import { Clock, MapPin } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 function PreviewMeeting({formValue}) {
   return (
-    <div>
-      {formValue?.eventName}
+    <div className='p-5 py-10 shadow-lg m-5 border-t-8'> 
+      <Image src={'/mcdonald-s-15-logo-svgrepo-com.svg'} alt="logo"
+        height={150} width={150}
+      />
+
+      <div className='grid grid-cols-1 md:grid-cols-3 mt-5'>
+        {/*Meeting Info*/}
+        <div className='p-4 border-r'>
+            <h2>User Name</h2>
+            <h2
+            className='font-bold text-2xl'
+            >{formValue?.eventName?formValue?.eventName:'Meeting Name'}</h2>
+            <div className='mt-5 flex-col gap-4'>
+              <h2 className='flex gap-2'><Clock/>{formValue?.duration} Min</h2>
+              <h2 className='flex gap-2'><MapPin/>{formValue?.setLocation} Meeting</h2>
+            </div>
+        </div>
+        {/*Meeting Info*/}
+        <div className='md:col-span-2'>
+
+        </div>
+
+      </div>
     </div>
   )
 }
