50f18cbdcb8051dd53d2d143839ad6c25224db04
Dhruv Ray <raydhruv25@gmail.com>
2025-02-27 09:01:13 -0600

    Adding of Availability Page

---
+++ app/(routes)/[business]/[meetingEventId]/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import MeetingTimeDateSelection from '../_components/MeetingTimeDateSelection'
+
+
+function SharedMeetingEvent() {
+
+    
+
+
+
+  return (
+    <div>
+      <MeetingTimeDateSelection/>
+    </div>
+  )
+}
+
+export default SharedMeetingEvent
---
+++ app/(routes)/[business]/_components/MeetingTimeDateSelection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MeetingTimeDateSelection() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MeetingTimeDateSelection
